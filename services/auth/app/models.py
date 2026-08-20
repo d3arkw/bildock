601ad43e67967f68c1ad38cd355aebbd26b5b30b
Denis <d3arkw@icloud.com>
2026-08-20 15:58:57 +0300

    feat: alembic setup with users migration in auth service

---
+++ services/auth/app/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import String, func, DateTime
+
+from bildock_lib.database import Base
+from sqlalchemy.orm import mapped_column, Mapped
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password_hash: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
